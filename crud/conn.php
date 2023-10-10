3b43ef61aaba2994cdd519db01ec4b2531553a80
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-10 20:14:57 +0300

    Delete crud/conn.php

--- crud/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-        $conn = mysqli_connect("localhost","crud_user","q1w2e3","crud");
-
-// Check connection
-        if (mysqli_connect_errno()){
-                        echo "Failed to connect DB : " . mysqli_connect_error();
-                }
-
-?>
