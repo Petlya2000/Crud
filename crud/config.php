0151740f337bb6ad3566af3224914914edade44b
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-10 20:32:14 +0300

    Update config.php

--- crud/config.php
+++ crud/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'crud_user');
-define('DB_PASSWORD', 'q1w2e3');
-define('DB_NAME', 'server');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
